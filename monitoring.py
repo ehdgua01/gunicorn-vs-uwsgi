34cf10c7428ddc6b495120c2f6ceefc48314b063
KDH <ehdgua01@naver.com>
2020-04-09 22:07:58 +0900

    ADD monitoring module

---
+++ monitoring.py
@@ -0,0 +1,33 @@
+import psutil
+from typing import Dict
+
+
+class Monitoring(object):
+    def __init__(self) -> None:
+        self.__stop = False
+
+    def measure_resource(self, pidfile: str) -> Dict[str, float]:
+        """CPU && RAM Usages"""
+        with open(pidfile) as f:
+            master_pid = f.read().strip()
+
+        master_process = psutil.Process(int(master_pid))
+        children = master_process.children()
+        cpu = 0
+        mem = 0
+
+        while not self.__stop:
+            cpu += master_process.cpu_percent()
+            mem += master_process.memory_percent()
+
+            for p in children:
+                cpu += p.cpu_percent()
+                mem += p.memory_percent()
+
+        return {
+            'cpu': cpu,
+            'mem': mem,
+        }
+
+    def stop(self) -> None:
+        self.__stop = True
