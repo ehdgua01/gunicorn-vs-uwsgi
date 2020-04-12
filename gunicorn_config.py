419c2c68d2ab0971a625003c3647a0b4a705f586
KDH <ehdgua01@naver.com>
2020-04-12 18:13:18 +0900

    UPDATE optimize gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,10 @@
 pidfile = 'gunicorn.pid'
 bind = [':80']
 
-workers = 4
+workers = 8
 worker_class = 'gevent'
 worker_connections = 4096
-threads = 4
+threads = 8
 
 preload_app = True
 keepalive = 0
